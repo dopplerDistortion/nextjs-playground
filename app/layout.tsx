0f1cf7620b96398d8dc3be91ad5ce38d8037499d
Göksenin Güngör <goksenin.gngr@gmail.com>
2023-10-28 19:29:02 +0300

    feat: shadcn config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
+import {ThemeProvider} from '@/components/theme/themeProvider';
 import '../styles/global.css';
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
       <body className="min-h-screen px-[env(safe-area-inset-top)]">
-        <main>{children}</main>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange>
+          <main>{children}</main>
+        </ThemeProvider>
       </body>
     </html>
   );
